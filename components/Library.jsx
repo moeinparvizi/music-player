e65214b74fb4cd844ec36b471579f4849e4a40b7
moeinparvizi <giventulip@gmail.com>
2023-11-22 23:45:16 +0330

    create library and modify about styles

--- components/Library.jsx
+++ components/Library.jsx
@@ -1,13 +1,25 @@
 "use client";
 
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai"
+
 const Library = () => {
   const onClick = () => {
-    console.log('click ahahaha')
-  }
+    // handle upload later
+    console.log('ai out line plus icon test');
+  };
 
   return (
-    <div onClick={onClick}>library</div>
+    <div className="flex flex-col">
+      <div className="flex items-center justify-between px-5 pt-4">
+        <div className="inline-flex items-center gap-x-2">
+          <TbPlaylist size={26} className="text-neutral-400" />
+          <p className="text-md font-medium text-neutral-400">Your Library</p>
+          <AiOutlinePlus size={26} className="text-neutral-400" onClick={onClick} />
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
 export default Library;
