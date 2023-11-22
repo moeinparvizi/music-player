9b397c64c3fd10eb49ee493e1f64e3ba65a52013
moeinparvizi <giventulip@gmail.com>
2023-11-23 01:33:18 +0330

    ListItem components
    add liked button and styles

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,11 +1,41 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
     <div className="flex flex-col bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
-        header
+        <div className="mb-2">
+          <h1 className="text-wite text-3xl font-semibold">
+            Welcome Back
+          </h1>
+          <div className="
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            xl:grid-cols-3
+            2xl:grid-cols-4
+            gap-3
+            mt-4
+          ">
+            <ListItem
+              image="/images/liked.png"
+              name="Like Songs"
+              href="liked"
+            />
+          </div>
+        </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white capitalize text-2xl font-semibold">
+            newest songs
+          </h1>
+        </div>
+        <div>
+          list of songs
+        </div>
+      </div>
     </div>
   );
 }
