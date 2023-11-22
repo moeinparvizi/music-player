95a24fd70933f994dcd1b4187192ab34d4b2cedf
moeinparvizi <giventulip@gmail.com>
2023-11-22 15:20:58 +0330

    start project

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import { Fragment } from 'react'
+
+export default function Home() {
+  return (
+    <Fragment>
+      <div className='text-green-500'>salam</div>
+    </Fragment>
+  )
+}
