89d8b494d2682424483f40cfe5bcc4923f4f1faa
moeinparvizi <giventulip@gmail.com>
2023-11-26 01:19:07 +0330

    add condition and check empty input

--- app/search/components/SearchContent.jsx
+++ app/search/components/SearchContent.jsx
@@ -1,12 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-const SearchContent = ({value}) => {
-  const [val, setVal] = useState(value)
+const SearchContent = ({ value }) => {
+  const [val, setVal] = useState("");
+  const [songs, setSongs] = useState([]);
+  const [flag, setFlag] = useState(true);
 
-  console.log(value);
-  return (
-    <div>your search is {value}</div>
-  );
-}
+  const url = "/text.JSON";
+
+  async function fetchSongs() {
+    const res = await fetch(url);
+    const song = await res.json();
+    setSongs(song);
+  }
+
+  useEffect(() => {
+    setVal(value);
+    fetchSongs();
+  }, [value]);
+
+  const valCondition = (item) => {
+    if (val != "") {
+      let result = item.title.search(val);
+      if (result == 0) {
+        return <div>{item.title}</div>;
+      }
+    }
+  };
+
+  // when empty input
+  if (val == "") {
+    return <div>you can search in up input</div>;
+  }
+
+  return <div>{songs.map((item) => valCondition(item))}</div>;
+};
 
 export default SearchContent;
