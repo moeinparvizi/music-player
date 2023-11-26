f0ffafd82c15319cd3a9b66b097bb48cf9bd7177
moeinparvizi <giventulip@gmail.com>
2023-11-27 00:21:21 +0330

    complate search result styles

--- app/search/components/SearchContent.jsx
+++ app/search/components/SearchContent.jsx
@@ -1,3 +1,4 @@
+import MediaItem from "@/components/MediaItem";
 import { useEffect, useState } from "react";
 
 const SearchContent = ({ value }) => {
@@ -20,19 +21,31 @@ const SearchContent = ({ value }) => {
 
   const valCondition = (item) => {
     if (val != "") {
-      let result = item.title.search(val);
-      if (result == 0) {
-        return <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">{item.title}</div>;
+      const result = item.title.search(val);
+      const result2 = item.artist.search(val);
+      const result3 = item.albume.search(val);
+      if (result == 0 || result2 == 0 || result3 == 0) {
+        return (
+          <div className="flex items-center gap-x-4 w-full">
+            {/* {item.title} */}
+            <MediaItem song={item} onClick={() => {}} />
+          </div>
+        );
       }
     }
   };
 
   // when empty input
   if (val == "") {
-    return <div>you can search in up input</div>;
+    return (
+      // ? change this value in div
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400 capitalize text-lg">
+        you can search in up input
+      </div>
+    );
   }
 
-  return <div>{songs.map((item) => valCondition(item))}</div>;
+  return <div className="flex flex-col gap-y-2 w-full px-6">{songs.map((item) => valCondition(item))}</div>;
 };
 
 export default SearchContent;
